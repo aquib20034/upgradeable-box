892be3752c971e144d15a77d8d6473502ff89b31
AliWisam <aliwisaam73@gmail.com>
2022-04-11 14:57:07 +0500

    adding upgradeable smart contrats

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -1,11 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
-import "erc721a/contracts/ERC721A.sol";
+// import "erc721a/contracts/ERC721A.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+// import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts-upgradeable/utils/cryptography/MerkleProofUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract Box is Initializable, ERC721A, OwnableUpgradeable{
+contract Box is Initializable, ERC721Upgradeable, OwnableUpgradeable{
     uint256 public constant MAX_SUPPLY = 7777;
     uint256 public constant MAX_PUBLIC_MINT = 10;
     uint256 public constant MAX_WHITELIST_MINT = 3;
@@ -26,9 +28,11 @@ contract Box is Initializable, ERC721A, OwnableUpgradeable{
     mapping(address => uint256) public totalPublicMint;
     mapping(address => uint256) public totalWhitelistMint;
 
+    event Init(string);
     function initialize() external initializer{
-        __ERC721A_init("Box","BOX");
+        __ERC721_init("Box","BOX");
         __Ownable_init();
+        emit Init("a");
     }
 
     modifier callerIsUser() {
@@ -40,7 +44,7 @@ contract Box is Initializable, ERC721A, OwnableUpgradeable{
 
     function mint(uint256 _quantity) external payable callerIsUser{
         require(publicSale, "Box :: Not Yet Active.");
-        require((totalSupply() + _quantity) <= MAX_SUPPLY, "Box :: Beyond Max Supply");
+        // require((totalSupply() + _quantity) <= MAX_SUPPLY, "Box :: Beyond Max Supply");
         require((totalPublicMint[msg.sender] +_quantity) <= MAX_PUBLIC_MINT, "Box :: Already minted 3 times!");
         require(msg.value >= (PUBLIC_SALE_PRICE * _quantity), "Box :: Payment is below the price ");
 
@@ -50,12 +54,12 @@ contract Box is Initializable, ERC721A, OwnableUpgradeable{
 
     function whitelistMint(bytes32[] memory _merkleProof, uint256 _quantity) external payable callerIsUser{
         require(whiteListSale, "Box :: Minting is on Pause");
-        require((totalSupply() + _quantity) <= MAX_SUPPLY, "Box :: Cannot mint beyond max supply");
+        // require((totalSupply() + _quantity) <= MAX_SUPPLY, "Box :: Cannot mint beyond max supply");
         require((totalWhitelistMint[msg.sender] + _quantity)  <= MAX_WHITELIST_MINT, "Box :: Cannot mint beyond whitelist max mint!");
         require(msg.value >= (WHITELIST_SALE_PRICE * _quantity), "Box :: Payment is below the price");
         //create leaf node
         bytes32 sender = keccak256(abi.encodePacked(msg.sender));
-        require(MerkleProof.verify(_merkleProof, merkleRoot, sender), "Box :: You are not whitelisted");
+        require(MerkleProofUpgradeable.verify(_merkleProof, merkleRoot, sender), "Box :: You are not whitelisted");
 
         totalWhitelistMint[msg.sender] += _quantity;
         _safeMint(msg.sender, _quantity);
--- scripts/upgradeBoxToV2.js
+++ scripts/upgradeBoxToV2.js
@@ -1,14 +1,16 @@
+/** @format */
+
 // scripts/deploy.js
-const PROXY = "0x65eb26fe3b30a3119b3a91e38289e426f23daff3";
+const PROXY = "0xA23dD8CDed1F05cB3EAb5D509e7D88304ba8307b";
 async function main() {
-    const Cntrct = await ethers.getContractFactory("BoxV2");
-    await upgrades.upgradeProxy(PROXY,Cntrct);
-    console.log("Contract upgraded:");
-  }
-  
-  main()
-    .then(() => process.exit(0))
-    .catch(error => {
-      console.error(error);
-      process.exit(1);
-    });
+  const Cntrct = await ethers.getContractFactory("BoxV2");
+  await upgrades.upgradeProxy(PROXY, Cntrct);
+  console.log("Contract upgraded:");
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
